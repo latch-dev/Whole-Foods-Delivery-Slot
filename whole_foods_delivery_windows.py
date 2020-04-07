dc26ffddb9b9b76ee66adae27295b4669f4ad218
latch-dev <63302484+latch-dev@users.noreply.github.com>
2020-04-07 13:00:02 -0400

    Update whole_foods_delivery_windows.py
    fixed driver location, adjusted timer and logs

--- whole_foods_delivery_windows.py
+++ whole_foods_delivery_windows.py
@@ -6,22 +6,23 @@ import sys
 import time
 
 import winsound
-
+from datetime import datetime
 
 def getWFSlot(productUrl):
-   driver = webdriver.Chrome()
-   driver.get(productUrl)           
+   driver = webdriver.Chrome("C:\\Users\\dines\\programs\\chromedriver_win32\\chromedriver.exe")
+   driver.get(productUrl)
    html = driver.page_source
    soup = bs4.BeautifulSoup(html)
-   time.sleep(60)
+   print("Will start sleeping at ",  datetime.now().time()," for 120 seconds to give you enough time to get to the schedule order shipping screen")
+   time.sleep(120)
    no_open_slots = True
 
-   duration = 1000
-   milliseconds freq = 440
+   duration = 20000
+   freq = 800
 
    while no_open_slots:
       driver.refresh()
-      print("refreshed")
+      print("Looping.  Refreshing at ",  datetime.now().time())
       html = driver.page_source
       soup = bs4.BeautifulSoup(html)
       time.sleep(4)
@@ -30,23 +31,23 @@ def getWFSlot(productUrl):
       try:
          next_slot_text = soup.find('h4', class_ ='ufss-slotgroup-heading-text a-text-normal').text
          if slot_pattern in next_slot_text:
-            print('SLOTS OPEN!')
+            print("Condition 1: FOUND OPEN SLOTS AT ",  datetime.now().time())
             winsound.Beep(freq, duration)
             no_open_slots = False
-            time.sleep(1400)
+            time.sleep(140000)
       except AttributeError:
          continue
 
       try:
          no_slot_pattern = 'No delivery windows available. New windows are released throughout the day.'
          if no_slot_pattern == soup.find('h4', class_ ='a-alert-heading').text:
-            print("NO SLOTS!")
+            print("No delivery windows available found at ",  datetime.now().time())
       except AttributeError: 
-            print('SLOTS OPEN!')
+            print("Condition 2: FOUND OPEN SLOTS AT ",  datetime.now().time())
             winsound.Beep(freq, duration)
+            time.sleep(140000)
             no_open_slots = False
 
 
 getWFSlot('https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1')
-
 
